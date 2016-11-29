dbf7b59194b409dd8bd30608e38c9a0fb7e28d76
Rafael Mazzoli Camargo <rafaelmazzoli@hotmail.com>
2016-11-29 18:14:31 -0200

    Add files via upload

--- TrabalhoPoo/src/trabalho/poo/TrabalhoPoo.java
+++ TrabalhoPoo/src/trabalho/poo/TrabalhoPoo.java
@@ -5,31 +5,10 @@
  */
 package trabalho.poo;
 
-import com.google.gson.Gson;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 public class TrabalhoPoo
 {
     public static void main(String[] args)
     {
-        Gson gson = new Gson();
-        String fileContent = null;
-        try {
-            fileContent = new String(Files.readAllBytes(Paths.get("datafile.json")));
-        } catch (IOException ex) {
-            Logger.getLogger(TrabalhoPoo.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        JData dataCollection = gson.fromJson(fileContent, JData.class);
-        
-        dataCollection.getData().stream().forEach((record) -> {
-            if(record.getValue() != 0)
-            {
-                System.out.println(record.getCountry() + ": " + record.getYear() + " -> " + record.getValue());
-            }
-        });
+        Interface aInterface = new Interface();
     }
 }
